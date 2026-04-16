6727cc6d1d301f595000016e4643fae935c35992
Budai Lili <csak.budai.lili@gmail.com>
2026-04-17 00:36:38 +0200

    GVLRPX - task 3 footer + header components

---
+++ react-projects/src/components/header/header.jsx
@@ -0,0 +1,34 @@
+const Header = () => {
+  return (
+    <header>
+      <h1>Web programozás-1 Előadás Házi feladat</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="../index.html">Főoldal</a>
+          </li>
+          <li>
+            <a href="../javascript-crud/javascript.html">Javascript CRUD</a>
+          </li>
+          <li>
+            <a href="react.html">React CRUD</a>
+          </li>
+          <li>
+            <a href="spa.html">SPA</a>
+          </li>
+          <li>
+            <a href="fetchapi.html">Fetch API</a>
+          </li>
+          <li>
+            <a href="axios.html">Axios CRUD</a>
+          </li>
+          <li>
+            <a href="../oojs/oojs.html">OOJS App</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
