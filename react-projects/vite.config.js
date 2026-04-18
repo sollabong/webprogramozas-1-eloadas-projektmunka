6e0072ff149ddda5515d11aebbd020207af2be27
Gabor Soltesz-Keri <solteszkerigabor@outlook.hu>
2026-04-18 23:32:19 +0200

    pathes fixed

--- react-projects/vite.config.js
+++ react-projects/vite.config.js
@@ -3,21 +3,21 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  root: '../',
-  server: {
-    open: '/index.html',
-    fs: {
-      allow: ['../'],
-    },
-  },
-  build: {
-    outDir: 'react-projects/dist',
-    rollupOptions: {
-      input: {
-        react: 'react.html',
-        // spa: 'spa.html',
-        // axios: 'axios.html',
+    root: '../',
+    server: {
+      open: '/index.html',
+      fs: {
+        allow: ['../'],
       },
     },
-  },
+    build: {
+      outDir: 'react-projects/dist',
+      rollupOptions: {
+        input: {
+          react: 'react.html',
+          spa: 'spa.html',
+          axios: 'axios.html',
+        },
+      },
+    },
 });
